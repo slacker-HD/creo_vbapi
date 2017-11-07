bc9d579f0a3c6e425538931c941624ee3018ef13
slacker_HD <hudi@linuxmail.org>
2017-11-07 23:14:20 +0800

    add chapter2.1

---
+++ chapter2.1/chapter2.1/Frm_load.vb
@@ -0,0 +1,13 @@
+﻿Public Class Frm_load
+    Private Sub Btn_new_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Btn_new.Click
+        If Creo_New() <> True Then
+            MsgBox("无法新建CREO对话！")
+        End If
+    End Sub
+
+    Private Sub Btn_Connect_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Btn_Connect.Click
+        If Creo_Connect() <> True Then
+            MsgBox("无法连接到CREO对话！")
+        End If
+    End Sub
+End Class
---
+++ chapter2.1/chapter2.1/Module_VB API.vb
@@ -0,0 +1,40 @@
+﻿Imports pfcls
+Imports System.Configuration
+
+Module Module_vbapi
+    Public asyncConnection As IpfcAsyncConnection = Nothing '全局变量，用于存储连接会话的句柄
+
+    '连接现有会话
+    Public Function Creo_Connect() As Boolean
+        Try
+            If asyncConnection Is Nothing OrElse Not asyncConnection.IsRunning Then
+                asyncConnection = (New CCpfcAsyncConnection).Connect(Nothing, Nothing, Nothing, Nothing)
+                Creo_Connect = True
+            End If
+        Catch ex As Exception
+            Creo_Connect = False
+            MsgBox(ex.Message.ToString + Chr(13) + ex.StackTrace.ToString)
+        End Try
+    End Function
+    '打开新会话
+    Public Function Creo_New() As Boolean
+        Try
+            Dim CmdLine As String = ConfigurationManager.AppSettings("CmdLine").ToString()
+            Dim TextPath As String = ConfigurationManager.AppSettings("TextPath").ToString()
+            asyncConnection = (New CCpfcAsyncConnection).Start(CmdLine, TextPath)
+            Creo_New = True
+        Catch ex As Exception
+            Creo_New = False
+            MsgBox(ex.Message.ToString + Chr(13) + ex.StackTrace.ToString)
+        End Try
+    End Function
+    '使用宏的方式刷新绘图
+    Public Sub M_Refresh()
+        Try
+            asyncConnection.Session.RunMacro("IMI ~ Command `ProCmdWinActivate` ;~ Command `ProCmdDwgRegenModel`;")
+        Catch ex As Exception
+            MsgBox(ex.Message.ToString + Chr(13) + ex.StackTrace.ToString)
+        End Try
+
+    End Sub
+End Module
